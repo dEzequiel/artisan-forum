2c6e0b2f5874f7f2f43f1e091a0a3f9f01711b62
Ezequiel De La Rosa <e.delarosa@innovation.es>
2023-02-08 17:01:49 +0100

    Stylish blade template

--- resources/views/post/form.blade.php
+++ resources/views/post/form.blade.php
@@ -1,76 +1,67 @@
-<!DOCTYPE html>
-<html>
-<head>
-    @vite('resources/css/app.css')
-    <style>
-        .alert-danger {
-            color: red;
-        }
-    </style>
 
-</head>
-<body>
 
-<h3><strong>Add Post</strong></h3>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Post') }}
+        </h2>
+    </x-slot>
 
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li style="color: red">{{ $error }}</li>
-            @endforeach
-        </ul>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="overflow-hidden sm:rounded-lg">
+                <form action="{{ route('post.store')  }}" method="POST">
+                    @csrf
+                    <!-- TITLE -->
+                        <label for="title" class="">Title</label><br>
+                        <input class="w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50" type="text" id="title" name="title" value="{{ old('title')  }}" ><br> <!-- required -->
+
+                    @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+
+                    <br>
+
+                    <!-- EXTRACT -->
+                    <label for="extract">Short Description</label><br>
+                    <input class="w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50" type="text" id="extract" name="extract" value="{{ old('extract')  }}"><br>
+
+                    @error('extract')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+
+                    <br>
+
+                    <!-- CONTENT -->
+                    <label for="content">Content</label><br>
+                    <textarea id="message" rows="10" class="w-full p-2 w-full rounded-lg border-gray-300" placeholder="Write your thoughts here..."></textarea>                    @error('body')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+
+                    <br>
+
+                    <!-- CHECKBOX -->
+                    <label><input type="checkbox" id="cboxcaducable" value="caducable"><strong>Caducable</strong></label>
+                    <label><input type="checkbox" id="cboxcomentable" value="comentable"><strong>Comentable</strong></label><br>
+
+                    <!-- SELECT -->
+                    <label>
+                        <select name="acceso">
+                            <option value="privado">Privado</option>
+                            <option value="Publico">Publico</option>
+                        </select>
+                    </label>
+
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
     </div>
-@endif
 
-<form action="{{ route('post.store')  }}" method="POST">
-    @csrf
-    <!-- TITLE -->
-    <label for="title">Title</label><br>
-    <input type="text" id="title" name="title" value="{{ old('title')  }}" ><br> <!-- required -->
+{{--
 
-    @error('title')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-
-    <br>
-
-    <!-- EXTRACT -->
-    <label for="extract">Short Description</label><br>
-    <input type="text" id="extract" name="extract" value="{{ old('extract')  }}"><br>
-
-    @error('extract')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-
-    <br>
-
-    <!-- CONTENT -->
-    <label for="content">Content</label><br>
-    <input type="text" id="body" name="body" value="{{ old('body')  }}"><br> <!-- required -->
-    @error('body')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-
-    <br>
-
-    <!-- CHECKBOX -->
-    <label><input type="checkbox" id="cboxcaducable" value="caducable"><strong>Caducable</strong></label>
-    <label><input type="checkbox" id="cboxcomentable" value="comentable"><strong>Comentable</strong></label><br>
-
-    <!-- SELECT -->
-    <label>
-        <select name="acceso">
-            <option value="privado">Privado</option>
-            <option value="Publico">Publico</option>
-        </select>
-    </label>
-
-    <input type="submit" value="Submit">
-</form>
-
-</body>
-</html>
+--}}
+</x-app-layout>
 
 
 
