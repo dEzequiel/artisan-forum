48c72d037ad8526080869956d53ddc9e39885cde
Ezequiel De La Rosa <e.delarosa@innovation.es>
2023-02-15 18:00:00 +0100

    Controller CRUD implementation

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -23,17 +23,48 @@ class CommunityController extends Controller
     }
 
     public function store(Request $request): JsonResponse
- {
+     {
 
-     $community = new Community;
-     $community->name = $request->input('name');
-     $community->description = $request->input('description');
+         $community = new Community;
+         $community->name = $request->input('name');
+         $community->description = $request->input('description');
 
-     try {
-         $community->save();
-     } catch (Exception $e) {
-         echo 'Caught exception: ',  $e->getMessage(), "\n";
+         try {
+             $community->save();
+         } catch (Exception $e) {
+             echo 'Caught exception: ',  $e->getMessage(), "\n";
+         }
+
+         return response()->json('Community created successfully!', 201);
      }
 
-     return response()->json('Community created successfully!', 201);
- }}
+     public function delete(Request $request): JsonResponse {
+
+        $communityId = $request->input('id');
+
+         try {
+             Community::query()->where('id', '=', $communityId)->delete();
+         } catch (Exception $e) {
+             echo 'Caught exception: ',  $e->getMessage(), "\n";
+         }
+
+         return response()->json('Community deleted successfully!', 200);
+     }
+
+     public function update(Request $request): JsonResponse {
+        $communityId = $request->input('id');
+
+        try {
+            Community::query()->where('id', '=', $communityId)->update([
+                'name' => $request->input('name'),
+                'description' => $request->input('description')
+            ]);
+
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+
+         return response()->json('Community updated successfully!', 200);
+
+     }
+}
