b5d7526d8a0a851481b0aa5c68eed1cb92a36952
Ezequiel De La Rosa <e.delarosa@innovation.es>
2023-02-17 22:47:43 +0100

    refactor: group related api routes coontroller and add name

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/community', [CommunityController::class, 'index']);
-Route::get('/community/getAll', [CommunityController::class, 'getAll']);
-Route::get('/community/{id}', [CommunityController::class, 'get']);
-Route::post('/community', [CommunityController::class, 'store']);
-Route::delete('/community', [CommunityController::class, 'delete']);
-Route::patch('/community', [CommunityController::class, 'update']);
+Route::controller(CommunityController::class)->group(function () {
+    Route::get('/community', 'index')->name('index');
+    Route::get('/community/getAll', 'getAll')->name('getAll');
+    Route::get('/community/{id}', 'get')->name('get');
+    Route::post('/community', 'store')->name('add');
+    Route::delete('/community', 'delete')->name('delete');
+    Route::patch('/community', 'update')->name('update');
+})->name('api.community');
+
+
