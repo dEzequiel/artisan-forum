16829f83e627bd9f00c725f9735d42932be24f1b
Ezequiel De La Rosa <e.delarosa@innovation.es>
2023-02-18 22:33:09 +0100

    test: define resource to represent community data response

---
+++ app/Http/Resources/ErrorResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ErrorResource extends JsonResource
+{
+    public int $code;
+    public ?string $message;
+
+    public function __construct(int $code, ?string $message)
+    {
+        $this->code = $code;
+        $this->message = $message;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'code' => $this->code,
+            'message' => $this->message,
+            'data' => null,
+        ];
+    }
+}
