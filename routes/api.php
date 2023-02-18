45d051a7276493f2763d6c5a234cd3a5f062f766
Ezequiel De La Rosa <e.delarosa@innovation.es>
2023-02-18 21:28:00 +0100

    feat: register community api routes with auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CommunityController;
+use App\Http\Controllers\TokenAdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,13 +20,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(CommunityController::class)->group(function () {
-    Route::get('/community', 'index')->name('index');
-    Route::get('/community/getAll', 'getAll')->name('getAll');
-    Route::get('/community/{id}', 'get')->name('get');
-    Route::post('/community', 'store')->name('add');
-    Route::delete('/community', 'delete')->name('delete');
-    Route::patch('/community', 'update')->name('update');
-})->name('api.community');
+Route::controller(CommunityController::class)
+    ->middleware('auth:sanctum')
+    ->group(function () {
+        Route::get('/community', 'index')->name('index');
+        Route::get('/community/getAll', 'getAll')->name('getAll');
+        Route::get('/community/{id}', 'get')->name('get');
+        Route::post('/community', 'store')->name('add');
+        Route::delete('/community', 'delete')->name('delete');
+        Route::patch('/community', 'update')->name('update');
+    })->name('api.community');
 
 
+Route::controller(TokenAdminController::class)->group(function () {
+    Route::post('/generate', 'generateToken')->name('generate');
+})->name('api.token');
